e35950552595ba1ff44d293dfab7829deaa1c4d7
Yelisz <yelizgulbahar0@gmail.com>
2024-08-27 23:44:23 +0300

    Connected

--- Context/DBcontext.cs
+++ Context/DBcontext.cs
@@ -5,9 +5,13 @@ namespace Disaster_Guide.Context
 {
     public class DBcontext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=FURKAN; database=DisasterGuideDB; integrated security=true; TrustServerCertificate=true");
+        }*/
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=YELIS\\SQLEXPRESS; database=DisasterGuideDB; integrated security=true; TrustServerCertificate=true");
         }
         public DbSet<TBLuser> users { get; set; }
     }
