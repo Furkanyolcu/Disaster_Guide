6419ee0e19e688e6b3431646586294248de4cabe
Furkanyolcu <119127899+Furkanyolcu@users.noreply.github.com>
2024-08-28 15:50:18 +0300

    Map file added

--- Context/DBcontext.cs
+++ Context/DBcontext.cs
@@ -5,14 +5,15 @@ namespace Disaster_Guide.Context
 {
     public class DBcontext : DbContext
     {
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("server=FURKAN; database=DisasterGuideDB; integrated security=true; TrustServerCertificate=true");
-        }*/
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=YELIS\\SQLEXPRESS; database=DisasterGuideDB; integrated security=true; TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("server=FURKAN; database=DisasterGuideDB; integrated security=true; TrustServerCertificate=true");
         }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=YELIS\\SQLEXPRESS; database=DisasterGuideDB; integrated security=true; TrustServerCertificate=true");
+        //}
         public DbSet<TBLuser> users { get; set; }
     }
 }
